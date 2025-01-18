6722ece4e3282f05e629d34b91bf1308b718fca2
Rishi <rishirandhawa@yahoo.com>
2025-01-18 22:42:14 +0000

    Update main app to use user routes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
+const userRoutes = require('./routes/users');
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.json({ message: 'Hello World!' });
 });
+
+app.use('/users', userRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
